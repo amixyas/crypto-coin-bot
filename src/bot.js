497f4ff4b5f9f7aa9663e319327c24ec0340b8c9
amixyas <i.amirat@esi-sba.dz>
2022-11-11 21:28:38 +0100

    update bot file

--- src/bot.js
+++ src/bot.js
@@ -11,11 +11,14 @@ const fs = require('fs')
 /* Before proceeding, we need to complete these steps :
 - Go to https://discord.com/developers/applications
 - Create new application and give it a name
-- G to Bot section and copy the Token then intilize the env var 'DISCORDJS_BOT_TOKEN' in .env file with this token
+- Go to Bot section, add new bot, copy the Token then intilize the env var 'DISCORDJS_BOT_TOKEN' in .env file with this token
 - Go to OAuth2 section and get the 'CLIENT ID'
 - Login to discord account and create new discord server with 'Add a Server'
 - Link the new server to new application :  https://discord.com/oauth2/authorized?client_id=<COPY_THE_CLIENT_ID_HERE>&scope=bot
 */
+
+
+
 
 console.log(process.env.DISCORDJS_BOT_TOKEN);
 
