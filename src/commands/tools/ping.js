0ab83c05c9c314d5177ab055ecf46ebeb72037cf
amixyas <i.amirat@esi-sba.dz>
2022-11-11 21:28:21 +0100

    create ping command

---
+++ src/commands/tools/ping.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require ('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Return my ping!'),
+    async execute(interaction, client) {
+        const message = await interaction.deferReply({
+            fetchReply: true
+        });
+
+        const newMessage = `API Latency: ${client.ws.ping}\nClient Ping: ${message.createdTimetamp - interaction.createdTimetamp}`;
+
+        await interaction.editReply({
+            content: newMessage
+        });
+    }
+}
